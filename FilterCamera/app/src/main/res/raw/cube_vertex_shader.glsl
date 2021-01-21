4b579979025e6c405c757811d7c176c4f511fa21
zyp <865791342@qq.com>
2021-01-21 21:52:08 +0800

    人脸贴图

--- FilterCamera/app/src/main/res/raw/cube_vertex_shader.glsl
+++
@@ -1,8 +0,0 @@
-attribute vec3 vPosition;
-uniform mat4 vMatrix;
-varying vec4 vColor;
-attribute vec4 aColor;
-void main() {
-    gl_Position = vMatrix * vec4(vPosition, 1.0);
-    vColor = aColor;
-}
